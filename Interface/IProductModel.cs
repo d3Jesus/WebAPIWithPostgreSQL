60193f6dd7ebd8fccf31ca9b718c19e89826ad8e
Yuran De Jesus <yuran_cumbane13@outlook.com>
2022-04-23 19:59:47 +0200

    Add project files.

---
+++ Interface/IProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIWithPostgreSQL.Models;
+
+namespace WebAPIWithPostgreSQL.Interface
+{
+    public interface IProductModel
+    {
+        List<ProductModel> GetProducts();
+
+        ProductModel GetProduct();
+
+        ProductModel CreateProduct(ProductModel productModel);
+
+        ProductModel UpdateProduct(ProductModel productModel);
+
+        ProductModel DeleteProduct(int productId);
+    }
+}
---
+++ Services/ProductService/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIWithPostgreSQL.Interface;
+using WebAPIWithPostgreSQL.Models;
+
+namespace WebAPIWithPostgreSQL.Services.ProductService
+{
+    public class ProductService : IProductModel
+    {
+        public ProductModel CreateProduct(ProductModel productModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductModel DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductModel GetProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductModel> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductModel UpdateProduct(ProductModel productModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
